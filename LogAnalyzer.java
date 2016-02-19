af972039306b9bd8953f56ebd476dc64717745d9
Sandra <72sandraloga@gmail.com>
2016-02-19 13:26:11 +0100

    cambiado el for por while en el metodo printHourlyCounts

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -44,9 +44,13 @@ public class LogAnalyzer
     public void printHourlyCounts()
     {
         System.out.println("Hr: Count");
-        for(int hour = 0; hour < hourCounts.length; hour++) {
-            System.out.println(hour + ": " + hourCounts[hour]);
+        int hour = 0;
+        while(hour < hourCounts.length)
+        {
+         System.out.println(hour + ": " + hourCounts[hour]);
+          hour++;
         }
+        
     }
     
     /**
